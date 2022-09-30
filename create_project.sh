5bb7c9690af5c56073c7e75cd85778220941628d
goddard-guryon <neaviduox1295@outlook.com>
2022-10-01 00:08:33 +0530

    add files

---
+++ create_project.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/bash
+
+# get details about project
+echo "Project Name: "
+read name
+if [[ $name == '' ]]; then
+    echo "Invalid filename"
+    exit 1
+fi
+pr_dir="$name/"
+echo "Project directory ($pr_dir): "
+read pr_new_dir
+if [[ $pr_new_dir != '' ]]; then
+    if [[ $pr_new_dir != */ ]]; then
+        pr_new_dir=$pr_new_dir'/'
+    fi
+    pr_dir=$pr_new_dir
+fi
+
+# create directories
+if ! command -v cmake &>/dev/null; then
+    echo "Failed to find cmake, make sure to install it before compiling the files"
+fi
+if ! command -v doxygen &>/dev/null; then
+    echo "Failed to find doxygen, make sure to install to generate code documentation"
+fi
+if ! command -v gdb &>/dev/null; then
+    echo "Failed to find gdb, make sure to install it for code debugging"
+fi
+if [[ -d $pr_dir ]]; then
+    echo "Project directory already exists, exiting"
+    exit 1
+fi
+echo "Initializing project"
+mkdir $pr_dir
+mkdir $pr_dir"build/"
+mkdir $pr_dir"docs/"
+cp CMakeLists.txt $pr_dir
+cp Doxyfile.in $pr_dir"docs/"
+cp mainpage.dox $pr_dir"docs/"
+cp main.f90 $pr_dir$name".f90"
+
+# Add project name in appropriate places
+sed -i -e "s/template_filename/$name/g" $pr_dir"CMakeLists.txt"
+sed -i -e "s/project_name/$name/g" $pr_dir"docs/Doxyfile.in"
+echo "Done"
---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/bash
+
+echo -e "Installation script to create new Fortran project template directories."
+echo -e "This script installs a command alias to quickly create template files for"
+echo -e "a Fortran project. Build files for CMake and Doxygen are added, along with"
+echo -e "a minimal main f90 file to begin working on the project.\n"
+echo -e "Make sure to not remove the file 'create_project.sh' from this directory."
+echo -e "Note: an extra alias can be added to quickly compile Fortran scripts through"
+echo -e "CMake. You can choose to add this alias in your bashrc file below:"
+echo "Add quick CMake compilation command alias (Y/n): "
+read consent
+if [[ $consent == '' ]]; then
+    consent='y'
+fi
+
+# we don't wanna keep adding aliases
+if command -v create_f90_project &>/dev/null; then
+    echo "Aliases have already been installed."
+else
+    echo -e "\n# Command aliases added via $(pwd)/install.sh script" >> ~/.bashrc
+    echo -e "alias create_f90_project=\"$(pwd)/create_project.sh\"" >> ~/.bashrc
+
+    # a handy alias I use for quick compilation
+    if [[ $consent == 'y' ]]; then
+        echo "alias compile_f90=\"cd build && cmake .. && make && find . -maxdepth 1 -type f -executable | xargs cp -t ../ && cd .. && find . -maxdepth 1 -type f -executable -exec {}\;\"" >> ~/.bashrc
+    fi
+    bash
+fi
+echo "Done"
+echo "For more information, check https://github.com/goddard-guryon/verbose-goggles\n"
