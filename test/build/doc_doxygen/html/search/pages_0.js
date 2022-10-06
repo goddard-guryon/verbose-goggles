27c951e44a374853721fd49804544f8324d4f045
goddard-guryon <neaviduox1295@outlook.com>
2022-10-06 21:11:20 +0530

    bug fix and dark mode for doxygen (yay)

--- test/build/doc_doxygen/html/search/pages_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['test_0',['test',['../index.html',1,'']]]
-];
--- test/test.f90
+++
@@ -1,7 +0,0 @@
-!> @brief Program documentation for main program
-!> Make sure to keep this documenation up to date.
-program main
-    implicit none
-
-    print '(A)', "Hello world!"
-end program main
